70c7e62922ef6bdd76e7207d754689407b646e55
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-01-11 21:38:21 +0100

    small changes

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ function numberOfCards() {
   albumCount.innerText = `Number Of Cards: ${album.length + 10}`;
 }
 
-window.onload = numberOfCards();
+function deleteChild() {
+  let section = document.querySelectorAll(".delete");
+  for (let i = 0; i < section.length; i++) {
+    section[i].parentElement.removeChild(section[i]);
+  }
+}
+
+function addBadge() {
+  let cards = document.querySelectorAll(".hot-deal");
+  for (let i = 0; i < cards.length; i++) {
+    let badge = document.createElement("span");
+    badge.classList.add("badge");
+    badge.classList.add("badge-danger");
+    badge.classList.add("position-absolute");
+    badge.innerText = "HOT";
+    cards[i].appendChild(badge);
+  }
+}
+
+function onLoad() {
+  numberOfCards();
+  addBadge();
+}
+
+window.onload = onLoad();
